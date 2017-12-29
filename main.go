400cabdd2a04d830432cd20507a1836ce2dd9e21
Codonal, Sergio <Sergio.Codonal@adidas-group.com>
2017-12-29 19:50:02 +0100

    main

--- main.go
+++ main.go
@@ -13,9 +13,10 @@ type FileTime struct {
 	ATime time.Time
 }
 
-
 func main() {
-	var dirPath string = "/Users/codonser/Documents/PERSONAL/fotos/mapi"
+	var dirPath string = "/Users/codonser/Documents/PERSONAL/fotos/test"
+	var destPath = "/Users/codonser/Documents/PERSONAL/fotos/test/2"
+
 	// walk all files in directory
 	extensions := []string{".mp4", ".jpeg",".avi"}
 	filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
@@ -25,8 +26,28 @@ func main() {
 					file, err := FTime(path)
 					if err == nil {
 						fmt.Println(path, "Mo Time", file.MTime)
+						fi, err := os.Stat(path)
+						if err != nil {
+							return nil
+						}
+						var mtime time.Time
+						mtime = fi.ModTime()
+						year, month, day := mtime.Date()
+						fmt.Println("Year : ", year)
+						fmt.Println("Month : ", int(month))
+						fmt.Println("Day : ", day)
+
+						if _, err := os.Stat(destPath+"/"+string(year)); os.IsNotExist(err) {
+							os.Mkdir(destPath+"/"+string(year), 0700)
+						}
 						//fmt.Println( "Cr Time", file.CTime)
 						//fmt.Println("Ac Time", file.ATime)
+
+						//NOW MOVING TO THE NEW PATH
+
+
+
+
 					}
 				}
 			}
@@ -48,4 +69,12 @@ func FTime(file string) (t *FileTime, err error) {
 	t.CTime = time.Unix(stat.Ctimespec.Sec, stat.Ctimespec.Nsec)
 	t.MTime = time.Unix(stat.Mtimespec.Sec, stat.Mtimespec.Nsec)
 	return
+}
+
+// exists returns whether the given file or directory exists or not
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil { return true, nil }
+	if os.IsNotExist(err) { return false, nil }
+	return true, err
 }
